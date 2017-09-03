872a8a77629c5b808dc11900b7e79609dd80630c
李彦争 <691630283@qq.com>
2017-09-03 22:03:37 +0800

    9.3

--- test.php
+++ test.php
@@ -1,3 +1,26 @@
 <?php
-	$url = "http://www.baidu.com/abc/de/fg.php?id=1";
-	echo basename($url);
+echo "<meta charset = 'utf-8'>";
+
+function myStrrev($str,$encoding='utf-8'){
+
+$newstr = '';
+
+$len = mb_strlen($str);
+
+for($i=$len-1; $i>=0; $i--){
+
+$newstr .= mb_substr($str,$i,1,$encoding);
+
+}
+
+return $newstr;
+
+}
+
+$string = '相信women是正确的。';
+
+echo $string;
+
+echo '<br />';
+
+echo myStrrev($string); // '的。的确正是nemow信相'
